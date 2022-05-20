fc6ae580311c6c81403fe010689323389cae75c7
Floyd Remmerswaal <floydremmerswaal@gmail.com>
2022-05-20 15:39:54 +0200

    searchFolder/Raster/Vector implemented

--- ellipsis/path/__init__.py
+++ ellipsis/path/__init__.py
@@ -11,19 +11,27 @@ s = session
 
 # read the docs
 
-def searchRaster(name = None, fuzzySearchOnName = False, root = ['myDrive', 'sharedWithMe', 'favorites'], firstPageOnly = True, bounds = None, userId= None, resolution=None,  dateFrom=None, dateTo= None, hashtag = None, token = None):
-    body = {'root': root, 'pageSize':20, 'type': ['raster']}
+def searchRaster(root = ['myDrive', 'sharedWithMe', 'favorites'], name = None, fuzzySearchOnName = False, userId = None, disabled = False, canView = None, pageSize = None, pageStart = None, hashtag = None, bounds = None, bands = None, resolution = None, dateFrom = None, dateTo = None, hasTimestamp = None, timestampSize = None, token = None):
+    body = {'root': root, 'type': ['raster']}
 
-def searchVector(name = None, root = ['myDrive', 'sharedWithMe', 'favorites'], hasVectorLayers = None, layerName = None, fuzzySearchOnLayerName = None, featureSize = None, seriesSize = None, messageSize = None, token = None):
-    body = {'root': root, 'pageSize':20, 'type': ['vector']}
+    dictAdd(body, "bands", bands)
+    dictAdd(body, "resolution", resolution)
+    dictAdd(body, "dateFrom", dateFrom)
+    dictAdd(body, "dateTo", dateTo)
+    dictAdd(body, "hasTimestamp", hasTimestamp)
+    dictAdd(body, "timestampSize", timestampSize)
 
-    dictAdd(body, "hasVectorLayers", hasVectorLayers)
-    dictAdd(body, "layerName", layerName)
-    dictAdd(body, "fuzzySearchOnLayerName", fuzzySearchOnLayerName)
-    dictAdd(body, "featureSize", featureSize)
-    dictAdd(body, "seriesSize", seriesSize)
-    dictAdd(body, "messageSize", messageSize)
-    
+    dictAdd(body, "root", root)
+    dictAdd(body, "name", name)
+    dictAdd(body, "fuzzySearchOnName", fuzzySearchOnName)
+    dictAdd(body, "userId", userId)
+    dictAdd(body, "disabled", disabled)
+    dictAdd(body, "canView", canView)
+    dictAdd(body, "pageSize", pageSize)
+    dictAdd(body, "pageStart", pageStart)
+    dictAdd(body, "hashtag", hashtag)
+    dictAdd(body, "bounds", bounds)
+
     keepGoing = True
     results = []
     while keepGoing:
@@ -43,7 +51,54 @@ def searchVector(name = None, root = ['myDrive', 'sharedWithMe', 'favorites'], h
 
         if firstPageOnly:
             keepGoing = False
-        if len(result) < 20:
+        if len(result) < pageSize:
+            keepGoing = False
+        results = results + result
+
+
+    return(results)
+
+def searchVector(root = ['myDrive', 'sharedWithMe', 'favorites'], name = None, fuzzySearchOnName = False, userId = None, disabled = False, canView = None, pageSize = None, pageStart = None, hashtag = None, bounds = None, hasVectorLayers = None, layerName = None, fuzzySearchOnLayerName = None, featureSize = None, seriesSize = None, messageSize = None, token = None, firstPageOnly = True)):
+    body = {'root': root, 'type': ['vector']}
+
+    dictAdd(body, "hasVectorLayers", hasVectorLayers)
+    dictAdd(body, "layerName", layerName)
+    dictAdd(body, "fuzzySearchOnLayerName", fuzzySearchOnLayerName)
+    dictAdd(body, "featureSize", featureSize)
+    dictAdd(body, "seriesSize", seriesSize)
+    dictAdd(body, "messageSize", messageSize)
+    
+    dictAdd(body, "root", root)
+    dictAdd(body, "name", name)
+    dictAdd(body, "fuzzySearchOnName", fuzzySearchOnName)
+    dictAdd(body, "userId", userId)
+    dictAdd(body, "disabled", disabled)
+    dictAdd(body, "canView", canView)
+    dictAdd(body, "pageSize", pageSize)
+    dictAdd(body, "pageStart", pageStart)
+    dictAdd(body, "hashtag", hashtag)
+    dictAdd(body, "bounds", bounds)
+
+    keepGoing = True
+    results = []
+    while keepGoing:
+        if token == None:
+            r = s.get(f"{URL}/path?{urllib.parse.urlencode(body)}")
+        else:
+            r = s.get(f"{URL}/path?{urllib.parse.urlencode(body)}", headers = {"Authorization":token} )
+        
+        if r.status_code != 200:
+            raise ValueError(r.text)
+            
+        result = r.json()
+        body['pageStart'] = result['nextPageStart']
+
+        results = results + result['result']
+        result = result['result']
+
+        if firstPageOnly:
+            keepGoing = False
+        if len(result) < pageSize:
             keepGoing = False
         results = results + result
 
@@ -52,18 +107,20 @@ def searchVector(name = None, root = ['myDrive', 'sharedWithMe', 'favorites'], h
 
     
 
-def searchFolder(name = None, fuzzySearchOnName = False, root = ['myDrive', 'sharedWithMe', 'favorites'], firstPageOnly = True, bounds = None, userId= None, resolution=None,  dateFrom=None, dateTo= None, hashtag = None, token = None):
-    body = {'root': root, 'pageSize':20, 'type': ['folder']}
-
-
+def searchFolder(root = ['myDrive', 'sharedWithMe', 'favorites'], name = None, fuzzySearchOnName = False, userId = None, disabled = False, canView = None, pageSize = None, pageStart = None, hashtag = None, bounds = None, token = None, firstPageOnly = True):
+    body = {'root': root, 'type': ['folder']}
+    
+    dictAdd(body, "root", root)
     dictAdd(body, "name", name)
     dictAdd(body, "fuzzySearchOnName", fuzzySearchOnName)
     dictAdd(body, "userId", userId)
-    dictAdd(body, "dateFrom", dateFrom)
-    dictAdd(body, "dateTo", dateTo)
-    dictAdd(body, "resolution", resolution)
+    dictAdd(body, "disabled", disabled)
+    dictAdd(body, "canView", canView)
+    dictAdd(body, "pageSize", pageSize)
+    dictAdd(body, "pageStart", pageStart)
     dictAdd(body, "hashtag", hashtag)
-    
+    dictAdd(body, "bounds", bounds)
+
     if bounds is not None:
         bounds = {'xMin':float(bounds['xMin']), 'xMax':float(bounds['xMax']), 'yMin':float(bounds['yMin']), 'yMax':float(bounds['yMax'])}
         body['bounds'] = bounds
@@ -94,7 +151,7 @@ def searchFolder(name = None, fuzzySearchOnName = False, root = ['myDrive', 'sha
 
     return(results)
 
-
+"""
 def searchPaths(name = None, fuzzySearchOnName = False, root = ['myDrive', 'sharedWithMe', 'favorites'], firstPageOnly = True, bounds = None, userId= None, resolution=None,  dateFrom=None, dateTo= None, hashtag = None, token = None):
     body = {'root': root, 'pageSize':20}
 
@@ -147,3 +204,4 @@ def searchPaths(name = None, fuzzySearchOnName = False, root = ['myDrive', 'shar
 
 
     return(results)
+"""
