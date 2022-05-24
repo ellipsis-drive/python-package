9361679c621037c2eb6916cc5d964614aa94c8b6
danielvandermaas <danielvandermaas@protonmail.com>
2022-05-24 12:57:57 +0200

    x

--- ellipsis/path/__init__.py
+++ ellipsis/path/__init__.py
@@ -3,8 +3,7 @@ from ellipsis import apiManager
 from ellipsis import sanitize
 
 def searchRaster(root = None, name = None, fuzzySearchOnName = False, userId = None, disabled = False, canView = None, pageStart = None, hashtag = None, bounds = None, bands = None, resolution = None, dateFrom = None, dateTo = None, hasTimestamp = None, timestampSize = None, token = None):
-
-
+    token = sanitize.validString('token', token, False)
     root = sanitize.validRootArray('root', root, False)    
     name = sanitize.validString('name', name, False)    
     fuzzySearchOnName = sanitize.validBool('fuzzySearchOnName', fuzzySearchOnName, True)
@@ -42,20 +41,58 @@ def searchRaster(root = None, name = None, fuzzySearchOnName = False, userId = N
         raise ValueError("When no token is given the root can only be ['public'']")
 
     body = {'root': root, 'name':name, 'fuzzySearchOnName':fuzzySearchOnName, 'userId':userId, 'disabled':disabled, 'canView':canView, 'pagestart':pageStart, 'hashtag':hashtag, 'bounds':bounds, 'bands':bands, 'resolution':resolution, 'dateFrom':dateFrom, 'dateTo':dateTo, 'hasTimestamp': hasTimestamp, 'timestampSize':timestampSize}
+    body['type'] = ['raster']
     r = apiManager.get('/path', body, token)
 
     return(r)
 
-##retrieving all pages depricated
-def searchVector(root = ['myDrive', 'sharedWithMe', 'favorites'], name = None, fuzzySearchOnName = False, userId = None, disabled = False, canView = None, pageSize = None, pageStart = None, hashtag = None, bounds = None, hasVectorLayers = None, layerName = None, fuzzySearchOnLayerName = None, featureSize = None, seriesSize = None, messageSize = None, token = None, firstPageOnly = True):
 
+def searchVector(root = None, name = None, fuzzySearchOnName = False, userId = None, disabled = False, canView = None, pageStart = None, hashtag = None, bounds = None, hasVectorLayers = None, layerName = None, fuzzySearchOnLayerName = None, token = None):
+    token = sanitize.validString('token', token, False)
+    root = sanitize.validRootArray('root', root, False)    
+    name = sanitize.validString('name', name, False)    
+    fuzzySearchOnName = sanitize.validBool('fuzzySearchOnName', fuzzySearchOnName, True)
+    userId = sanitize.validUuid('userId', userId, False)
+    disabled = sanitize.validBool('disabled', disabled, True)
+    canView = sanitize.validBool('canView', canView, False)
+    pageStart = sanitize.validUuid('pageStart', pageStart, False)
+    hashtag = sanitize.validString('hashtag', hashtag, False)
+    bounds = sanitize.validBounds('bounds', bounds, False)
+    hasVectorLayers = sanitize.validBool('hasVectorLayers', hasVectorLayers, False)
+    layerName = sanitize.validString('layerName', layerName, False)    
+    fuzzySearchOnLayerName = sanitize.validBool('fuzzySearchOnLayerName', fuzzySearchOnLayerName, False)    
+    
+    if type(root) != type(None) and token == None and (len(root) > 1  or root[0] != 'public'):
+        raise ValueError("When no token is given the root can only be ['public'']")
 
+    body = {'root': root, 'name':name, 'fuzzySearchOnName':fuzzySearchOnName, 'userId':userId, 'disabled':disabled, 'canView':canView, 'pagestart':pageStart, 'hashtag':hashtag, 'bounds':bounds, 'hasVectorLayers': hasVectorLayers, 'layerName':layerName, 'fuzzySearchOnLayerName':fuzzySearchOnLayerName}
+    body['type'] = ['vector']
 
-    return(None)
+    r = apiManager.get('/path', body, token)
+
+    return(r)
+
 
     
 ##retrieving all pages depricated
-def searchFolder(root = ['myDrive', 'sharedWithMe', 'favorites'], name = None, fuzzySearchOnName = False, userId = None, disabled = False, canView = None, pageSize = None, pageStart = None, hashtag = None, bounds = None, token = None, firstPageOnly = True):
+def searchFolder(root = None, name = None, fuzzySearchOnName = False, userId = None, pageStart = None, token = None):
+    token = sanitize.validString('token', token, False)
+    root = sanitize.validRootArray('root', root, False)    
+    name = sanitize.validString('name', name, False)    
+    fuzzySearchOnName = sanitize.validBool('fuzzySearchOnName', fuzzySearchOnName, True)
+    userId = sanitize.validUuid('userId', userId, False)
+    pageStart = sanitize.validUuid('pageStart', pageStart, False)
+
+    if type(root) != type(None) and token == None and (len(root) > 1  or root[0] != 'public'):
+        raise ValueError("When no token is given the root can only be ['public'']")
+
+    body = {'root': root, 'name':name, 'fuzzySearchOnName':fuzzySearchOnName, 'userId':userId, 'pagestart':pageStart}
+    body['type'] = ['folder']
+
+    r = apiManager.get('/path', body, token)
+
+    return(r)
+
     return(None)
 
 
