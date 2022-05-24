d1c37ebdf744fa75a0ab198a546040906b9c3080
Jelle Keulemans <jelle.keulemans@gmail.com>
2022-05-24 16:20:55 +0200

    implemented hashtag

--- ellipsis/path/__init__.py
+++ ellipsis/path/__init__.py
@@ -1,4 +1,1 @@
-from ellipsis.path.root import searchFolder
-from ellipsis.path.root import searchRaster
-from ellipsis.path.root import searchVector
-
+from ellipsis.path.root import searchFolder, searchRaster, searchVector, listFiles, listFolders, delete, editMetaData, movePaths, rename, setTrashed, get
--- ellipsis/path/hashtag/__init__.py
+++ ellipsis/path/hashtag/__init__.py
@@ -1,10 +1,1 @@
-from ...util import *
-
-def add():
-    pass
-
-def remove():
-    pass
-
-def search():
-    pass
+from root import add, remove, search
